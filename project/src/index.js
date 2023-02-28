d634575f1320c226db85df63a230ef1210629bd0
wangkodok <wangkodok@gmail.com>
2023-02-28 11:48:50 +0900

    메인 페이지 경로 수정

--- project/src/index.js
+++ project/src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <BrowserRouter basename="">
+  <BrowserRouter basename="/book-project">
     <App />
   </BrowserRouter>
   // </React.StrictMode>
