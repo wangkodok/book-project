6ec9de056c451f348c1718b25be9d769aa1bdc25
wangkodok <wangkodok@gmail.com>
2023-03-28 18:55:09 +0900

    리덕스 세팅

--- project/src/index.js
+++ project/src/index.js
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/State";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <BrowserRouter basename="/">
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter basename="/">
+      <App />
+    </BrowserRouter>
+  </Provider>
   // </React.StrictMode>
 );
