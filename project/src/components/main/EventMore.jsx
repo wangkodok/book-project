9bdd264941e79697f922a32683b8e754a794e6d9
wangkodok <wangkodok@gmail.com>
2023-04-21 17:50:41 +0900

    이벤트 배너 슬라이더 swiper 사용하여 구현

--- project/src/components/main/EventMore.jsx
+++ project/src/components/main/EventMore.jsx
@@ -1,23 +1,61 @@
 import { Link } from "react-router-dom";
 import styled from "../../style/main/EventMore.module.css";
 
+// 배너 이미지
+import EventImg00 from "../../../src/assets/event_slider_banner_00.jpg";
+import EventImg01 from "../../../src/assets/event_slider_banner_01.jpg";
+
+// 스와이퍼 리액트 컴포넌트, CSS
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+SwiperCore.use([Navigation, Pagination, Autoplay]);
+
 function EventMore() {
   return (
     <section>
       <div className={styled.inner}>
         <h1 className={styled.title}>이벤트</h1>
 
-        {/* banner slider */}
-        <div className={styled.bannerSlider}>
-          <ul className={styled.EventMoreList}>
-            <li>
-              <Link to="">
+        {/* 이벤트 배너 슬라이더 */}
+        <div>
+          <Swiper
+            className={styled.swiperList}
+            slidesPerView={1}
+            onSlideChange={() => console.log("slide change")}
+            onSwiper={(swiper) => console.log(swiper)}
+            loop={true}
+            // autoplay={true}
+            autoplay={{
+              delay: 3000,
+              disableOnInteraction: false,
+            }}
+          >
+            <SwiperSlide className={styled.swiperItem}>
+              <div>
                 <figure>
-                  <img src="https://via.placeholder.com/1200x200.jpg" alt="" />
+                  <img
+                    styled={styled.bannerImg}
+                    src={`${EventImg00}/`}
+                    alt="챗 GPT 사용해보세요."
+                  />
                 </figure>
-              </Link>
-            </li>
-          </ul>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className={styled.swiperItem}>
+              <div>
+                <figure styled={styled.bannerArea}>
+                  <img
+                    styled={styled.bannerImg}
+                    src={`${EventImg01}`}
+                    alt="신규 회원가입 할인쿠폰 증정"
+                  />
+                </figure>
+              </div>
+            </SwiperSlide>
+          </Swiper>
         </div>
       </div>
     </section>
