99a0e9dc68a16c8b259289c7abc15acba5c95750
wangkodok <wangkodok@gmail.com>
2023-05-12 00:48:28 +0900

    책 검색 결과 목록에 코드 추가 그 외 자잘한 코드 수정
    책 목록에 "품절" 배지 추가
    책 목록에 "품절" 타이틀 추가
    책 목록에 표지가 없으면 임시 이미지 추가
    그 외 자잘한 코드 수정

--- project/src/components/main/BookSearchResult.jsx
+++ project/src/components/main/BookSearchResult.jsx
@@ -10,6 +10,20 @@ import NoSearchResults from "components/main/NoSearchResults";
 // CSS 모듈
 import styled from "style/main/BookSearchResult.module.css";
 
+// 책 검색 후 목록에 나오는 배지
+function Price({ bookData, i }) {
+  if (bookData[i].sale_price < 0) {
+    if (bookData[i].price.toString() > 20000) {
+      return <strong className={styled["sold-out"]}>품절</strong>;
+    }
+    return <strong className={styled["sold-out"]}>품절</strong>;
+  }
+
+  if (bookData[i].price.toString() > 20000) {
+    return <strong className={styled.md}>MD추천</strong>;
+  }
+}
+
 function BookSearchResult({ save, bookData, addOnClick, convertPrice }) {
   const dispatch = useDispatch();
 
@@ -21,22 +35,35 @@ function BookSearchResult({ save, bookData, addOnClick, convertPrice }) {
             return (
               <li className={styled.item} key={i}>
                 <figure>
-                  <img src={bookData[i].thumbnail} alt={bookData[i].title} />
+                  <img
+                    src={
+                      bookData[i].thumbnail === ""
+                        ? "https://via.placeholder.com/120x174"
+                        : bookData[i].thumbnail
+                    }
+                    alt={bookData[i].title}
+                  />
                 </figure>
                 <div className={styled.boxContent}>
                   <strong className={styled.title}>{bookData[i].title}</strong>
                   <p className={styled.bookTitle}>
-                    책 소개: {bookData[i].contents}
+                    {" "}
+                    {bookData[i].contents === ""
+                      ? null
+                      : `책 소개: ${bookData[i].contents}`}
                   </p>
                   <p className={styled.price}>
                     정상가격: <em>{`${convertPrice(bookData[i].price)}원`}</em>
                   </p>
                   <p className={styled.sale_price}>
-                    할인가격:{" "}
-                    <em>{`${convertPrice(bookData[i].sale_price)}원`}</em>
+                    <em>
+                      {bookData[i].sale_price < 0
+                        ? null
+                        : `할인가격: ${convertPrice(bookData[i].sale_price)}원`}
+                    </em>
                   </p>
                   <p className={styled.authors}>
-                    저자: {bookData[i].authors[i]}
+                    저자: {bookData[i].authors[0]}
                   </p>
                   <p className={styled.datetime}>
                     최초 발행일:{" "}
@@ -52,20 +79,34 @@ function BookSearchResult({ save, bookData, addOnClick, convertPrice }) {
                   </p>
                   <div className={styled.sale}>
                     <button>구매하기</button>
-                    <button
-                      onClick={() => {
-                        dispatch(setCartListAdd(bookData[i]));
-                      }}
-                    >
-                      찜하기
-                    </button>
+                    {bookData[i].sale_price > 0 ? (
+                      <button
+                        onClick={() => {
+                          dispatch(setCartListAdd(bookData[i]));
+                        }}
+                      >
+                        찜하기
+                      </button>
+                    ) : (
+                      <em className={styled["item-sold-out"]}>품절</em>
+                    )}
                   </div>
                 </div>
 
+                <Price bookData={bookData} i={i} />
+
                 {/* MD 추천 */}
-                {`${bookData[i].price.toString()}` > 20000 ? (
+                {/* {`${bookData[i].price.toString()}` > 20000 &&
+                `${bookData[i].sale_price}` < 0 ? (
+                  <strong className={styled["sold-out"]}>품절</strong>
+                ) : (
                   <strong className={styled.md}>MD추천</strong>
-                ) : null}
+                )} */}
+
+                {/* 품절 */}
+                {/* {`${bookData[i].sale_price}` < 0 ? (
+                  <strong className={styled.md}>품절</strong>
+                ) : null} */}
               </li>
             );
           })}
