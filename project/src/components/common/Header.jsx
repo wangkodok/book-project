4020c5c4d534ca12e13d1fe1970bd1e8c7104f58
wangkodok <wangkodok@gmail.com>
2023-05-31 10:19:45 +0900

    헤더 GNB 수정

--- project/src/components/common/Header.jsx
+++ project/src/components/common/Header.jsx
@@ -1,16 +1,36 @@
 // 라우터
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
 // 스타일 컴포넌트
 import styled from "styled-components";
 
 function Header() {
+  const [menuClick, setMenuClick] = useState(false);
+  const clickRef = useRef();
+
+  useEffect(() => {
+    const handleOutsideClose = (e) => {
+      // useRef current 에 담긴 엘리먼트 바깥을 클릭 시 드롭메뉴 닫힘
+      if (menuClick && !clickRef.current.contains(e.target))
+        setMenuClick(false);
+    };
+    document.addEventListener("click", handleOutsideClose);
+
+    return () => document.removeEventListener("click", handleOutsideClose);
+  }, [menuClick]);
+
   return (
     <SectionHeader>
       <h1 className="blind">도서 검색 홈페이지</h1>
       <Inner>
-        <Link to="/"></Link>
-        <Gnb>
+        <Link
+          to="/"
+          onClick={() => {
+            setMenuClick(false);
+          }}
+        ></Link>
+        {/* <Gnb>
           <List className="list">
             <Item>
               <Link to="/best">베스트셀러</Link>
@@ -22,7 +42,49 @@ function Header() {
               <Link to="/ShoppingBasket">찜목록</Link>
             </Item>
           </List>
-        </Gnb>
+        </Gnb> */}
+        <Hamburger>
+          <MenuButton
+            ref={clickRef}
+            onClick={() => {
+              if (menuClick) {
+                setMenuClick(false);
+              } else {
+                setMenuClick(true);
+              }
+            }}
+          >
+            메뉴 보기
+          </MenuButton>
+          {menuClick === true ? (
+            <HamburgerMenu className="on">
+              <div>
+                <ul>
+                  <li>
+                    <Link
+                      to="/event"
+                      onClick={() => {
+                        setMenuClick(false);
+                      }}
+                    >
+                      이벤트
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/ShoppingBasket"
+                      onClick={() => {
+                        setMenuClick(false);
+                      }}
+                    >
+                      찜목록
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </HamburgerMenu>
+          ) : null}
+        </Hamburger>
       </Inner>
     </SectionHeader>
   );
@@ -51,20 +113,65 @@ let Inner = styled.div`
   }
 `;
 
-let Gnb = styled.nav``;
+// 헤더 레이아웃 이전 꺼
+// let Gnb = styled.nav``;
 
-let List = styled.ul`
-  display: flex;
+// let List = styled.ul`
+//   display: flex;
+// `;
+
+// let Item = styled.li`
+//   margin: 0 24px 0 0;
+
+//   > a {
+//     font-size: 14px;
+//   }
+
+//   :last-child {
+//     margin: 0;
+//   }
+// `;
+
+// 헤더 메뉴 버튼
+let Hamburger = styled.div`
+  position: relative;
 `;
 
-let Item = styled.li`
-  margin: 0 24px 0 0;
+let MenuButton = styled.button`
+  padding: 10px;
+  border: 1px solid #000;
+`;
 
-  > a {
-    font-size: 14px;
+let HamburgerMenu = styled.div`
+  position: absolute;
+  top: calc(100% - 1px);
+  right: 0;
+  left: 0;
+  z-index: 1000;
+  border: 1px solid #000;
+  background-color: #fff;
+
+  div {
   }
 
-  :last-child {
-    margin: 0;
+  ul {
+  }
+
+  li {
+    color: #fff;
+
+    &:last-child {
+      margin: 0;
+    }
+  }
+
+  a {
+    padding: 4px 0;
+    text-align: center;
+
+    &:hover {
+      color: #fff;
+      background-color: #000;
+    }
   }
 `;
