b4105b143e74dd2fd14bbe7c31dd2804f0da9cbb
wangkodok <wangkodok@gmail.com>
2023-06-06 21:57:02 +0900

    로그인 UI 초기 레이아웃 구현

---
+++ project/src/pages/Login.jsx
@@ -0,0 +1,129 @@
+import styled from "styled-components";
+
+function Login() {
+  return (
+    <LoginContent>
+      <div className="login-text-area">
+        <h1 className="title">로그인</h1>
+        <p className="title-desc">로그인 후 이용하세요.</p>
+      </div>
+      <div className="login-input-area">
+        <form>
+          <fieldset>
+            <input type="hidden" value="로그인" />
+            <label htmlFor="user-id" id="id">
+              <input
+                type="text"
+                id="user-id"
+                size="10"
+                placeholder="아이디를 입력하세요."
+              />
+            </label>
+            <label htmlFor="user-pw" id="pw">
+              <input
+                type="password"
+                id="user-pw"
+                size="10"
+                placeholder="비밀번호를 입력해주세요."
+              />
+            </label>
+            <button className="login-button">로그인</button>
+          </fieldset>
+        </form>
+        <div className="util-login">
+          <ul className="list">
+            <li className="item">
+              <button className="find">아이디 찾기</button>
+            </li>
+            <li className="item">
+              <button className="find">비밀번호 찾기</button>
+            </li>
+            <li className="item">
+              <button className="find">회원가입</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </LoginContent>
+  );
+}
+
+export default Login;
+
+const LoginContent = styled.div`
+  width: 320px;
+  margin: 0 auto;
+  padding: 20px;
+  box-sizing: border-box;
+
+  /* 로그인 영역의 타이틀 */
+  .login-text-area {
+    margin: 0 0 20px 0;
+    text-align: center;
+
+    .title {
+      margin: 0 0 8px 0;
+      font-size: 24px;
+      font-weight: 700;
+    }
+    .title-desc {
+      font-size: 16px;
+    }
+  }
+
+  /* 로그인 영역의 id/pw 입력 창 */
+  .login-input-area {
+    label {
+      display: block;
+      margin: 0 0 8px 0;
+      font-size: 20px;
+    }
+
+    input {
+      width: 100%;
+      padding: 8px 10px;
+      box-sizing: border-box;
+    }
+
+    .login-button {
+      width: 100%;
+      padding: 10px 0;
+      font-weight: 700;
+      color: #ffffff;
+      background-color: #b8883b;
+    }
+  }
+
+  /* 아이디/비밀번호 찾기, 회원가입 */
+  .util-login {
+    margin: 20px 0 0 0;
+
+    .list {
+      display: flex;
+      justify-content: center;
+
+      .item {
+        position: relative;
+      }
+
+      .item button {
+        font-size: 14px;
+      }
+
+      .item + .item {
+        margin: 0 0 0 12px;
+      }
+
+      .item + .item::before {
+        content: "";
+        position: absolute;
+        top: 50%;
+        left: -6px;
+        width: 1px;
+        height: 12px;
+        transform: translate(0, -50%);
+        background-color: #000;
+      }
+    }
+  }
+`;
--- project/src/pages/ShoppingBasket.jsx
+++ project/src/pages/ShoppingBasket.jsx
@@ -6,6 +6,7 @@ import SubPageTitle from "components/common/SubPageTitle";
 import ProductInfo from "pages/ProductInfo";
 import ProductCount from "pages/ProductCount";
 import ProductTotalPrice from "pages/ProductTotalPrice";
+import Login from "pages/Login";
 
 // CSS 모듈
 import styled from "style/cart/ShoppingBasket.module.css";
@@ -18,53 +19,62 @@ function SalePage({ convertPrice }) {
 
   return (
     <section>
-      <div className={styled.inner}>
-        {SubPageTitle("찜목록들", "찜목록을 확인해주세요.")}
-      </div>
-      <div className={styled.content}>
-        <div className={styled.list}>
-          <table>
-            {cartList.length === 0 ? null : (
-              <thead>
-                <tr>
-                  <th style={{ width: "50%", padding: "10px 0" }}>상품 정보</th>
-                  <th style={{ width: "20%", padding: "10px 0" }}>수량</th>
-                  <th style={{ width: "20%", padding: "10px 0" }}>금액</th>
-                  <th style={{ width: "10%", padding: "10px 0" }}>배송비</th>
-                </tr>
-              </thead>
-            )}
-            {cartList.map((value, i) => {
-              return (
-                <tbody key={i} className={styled.item}>
+      {cartList.length === 0 ? (
+        <Login />
+      ) : (
+        <>
+          <div className={styled.inner}>
+            {SubPageTitle("찜목록들", "찜목록을 확인해주세요.")}
+          </div>
+          <div className={styled.content}>
+            <div className={styled.list}>
+              <table>
+                <thead>
                   <tr>
-                    <td>
-                      {/* 상품 정보 */}
-                      <ProductInfo cartList={cartList} i={i} />
-                    </td>
-                    <td>
-                      {/* 수량 */}
-                      <ProductCount cartList={cartList} i={i} />
-                    </td>
-                    <td>
-                      {/* 금액 */}
-                      <ProductTotalPrice
-                        cartList={cartList}
-                        i={i}
-                        convertPrice={convertPrice}
-                      />
-                    </td>
-                    <td>
-                      {/* 배송비 */}
-                      <div className={styled["product-common"]}>무료 배송</div>
-                    </td>
+                    <th style={{ width: "50%", padding: "10px 0" }}>
+                      상품 정보
+                    </th>
+                    <th style={{ width: "20%", padding: "10px 0" }}>수량</th>
+                    <th style={{ width: "20%", padding: "10px 0" }}>금액</th>
+                    <th style={{ width: "10%", padding: "10px 0" }}>배송비</th>
                   </tr>
-                </tbody>
-              );
-            })}
-          </table>
-        </div>
-      </div>
+                </thead>
+
+                {cartList.map((value, i) => {
+                  return (
+                    <tbody key={i} className={styled.item}>
+                      <tr>
+                        <td>
+                          {/* 상품 정보 */}
+                          <ProductInfo cartList={cartList} i={i} />
+                        </td>
+                        <td>
+                          {/* 수량 */}
+                          <ProductCount cartList={cartList} i={i} />
+                        </td>
+                        <td>
+                          {/* 금액 */}
+                          <ProductTotalPrice
+                            cartList={cartList}
+                            i={i}
+                            convertPrice={convertPrice}
+                          />
+                        </td>
+                        <td>
+                          {/* 배송비 */}
+                          <div className={styled["product-common"]}>
+                            무료 배송
+                          </div>
+                        </td>
+                      </tr>
+                    </tbody>
+                  );
+                })}
+              </table>
+            </div>
+          </div>
+        </>
+      )}
     </section>
   );
 }
