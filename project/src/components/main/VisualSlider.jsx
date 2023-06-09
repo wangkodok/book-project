fce6cc397cfd2bb74131d51808dec596c445140c
wangkodok <wangkodok@gmail.com>
2023-06-09 15:28:23 +0900

    기존의 메인 비주얼 배너 새로운 배너로 변경하고 코드 변경

--- project/src/components/main/VisualSlider.jsx
+++ project/src/components/main/VisualSlider.jsx
@@ -1,20 +1,30 @@
 // 이미지 불러오기
-import ApplePay from "assets/main_visual_slider_00.png";
+// import ApplePay from "assets/main_visual_slider_00.png";
 // import Blockchain from "assets/main_visual_slider_01.png";
+import AiBanner from "assets/main_visual_banner_ai.jpg";
+import styled from "styled-components";
 
-// CSS 모듈
-import styled from "style/main/VisualSlider.module.css";
-
-function MainVisualSlider() {
+export default function MainVisualSlider() {
   return (
-    <section className={styled.visual}>
-      <div className={styled.content}>
+    <SectionVisual>
+      <div className="inner">
+        <h2 className="blind">메인 페이지 배너</h2>
         <figure>
-          <img src={ApplePay} alt="" />
+          <img
+            src={AiBanner}
+            alt="가상화폐의 주역, 블록체인 기술 컴퓨터 기술 기반의 데이터 위변조 방지 기술을 배워보세요."
+          />
         </figure>
       </div>
-    </section>
+    </SectionVisual>
   );
 }
 
-export default MainVisualSlider;
+const SectionVisual = styled.section`
+  background-color: #646682;
+
+  .inner {
+    width: 1320px;
+    margin: 0 auto;
+  }
+`;
