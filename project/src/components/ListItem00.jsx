e1a51552f6b10b6233d07f655f080678a86317d7
wangkodok <wangkodok@gmail.com>
2023-06-30 21:07:41 +0900

    코드 정리
    이미지 컴포넌트 재사용
    이미지 파일명 수정
    프롭스 수정

--- project/src/components/ListItem00.jsx
+++ project/src/components/ListItem00.jsx
@@ -1,18 +1,17 @@
 // 이미지
-import BookMoth from "assets/book_moth.jpg";
-import BookMoth01 from "assets/book_moth02.jpg";
-import BookMoth02 from "assets/book_moth03.jpg";
+import BookMoth_00 from "assets/book_moth_00.jpg";
+import BookMoth_01 from "assets/book_moth_01.jpg";
+import BookMoth_02 from "assets/book_moth_02.jpg";
 
 // 컴포넌트
 import BookRating from "components/BookRating";
+import Image from "components/ui/atoms/Image";
 
 function ListItem00() {
   return (
     <div className="common-month month">
       <div className="info-wrap">
-        <figure className="moth-book">
-          <img src={BookMoth} alt="" />
-        </figure>
+        <Image src={BookMoth_00}></Image>
         <div className="book-info">
           <h4 className="tit-h4">자바스크립트 핵심 가이드(더글...</h4>
           <BookRating score="3.5" />
@@ -30,9 +29,7 @@ function ListItem01() {
   return (
     <div className="common-month month">
       <div className="info-wrap">
-        <figure className="moth-book">
-          <img src={BookMoth02} alt="" />
-        </figure>
+        <Image src={BookMoth_01}></Image>
         <div className="book-info">
           <h4 className="tit-h4">더 괜찮은 웹 개발자가 되기 위한...</h4>
           <BookRating score="3.5" />
@@ -50,9 +47,7 @@ function ListItem02() {
   return (
     <div className="common-month month">
       <div className="info-wrap">
-        <figure className="moth-book">
-          <img src={BookMoth01} alt="" />
-        </figure>
+        <Image src={BookMoth_02}></Image>
         <div className="book-info">
           <h4 className="tit-h4">비전공자를 위한 이해할 수 있는 IT 지식</h4>
           <BookRating score="3.5" />
